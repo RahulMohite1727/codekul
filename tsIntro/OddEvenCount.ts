7ba1004b8ae8c49355fafde19a2574f63aaf0c94
SuperBuddy <36763574+SuperBuddys@users.noreply.github.com>
2018-09-30 19:03:41 +0530

    OddEvenCount

---
+++ tsIntro/OddEvenCount.ts
@@ -0,0 +1,15 @@
+var even =0;
+var odd = 0;
+for(var i=1; i<=100;i++){
+    if(i%2 == 0){
+
+        even++;
+    }
+    else{
+        odd++;
+    }
+ 
+}
+
+console.log("even count:"+even);
+console.log("odd count:"+odd);
