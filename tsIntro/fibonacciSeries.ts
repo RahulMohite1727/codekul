d29dfbd8958772172ff4ff3d02f1d37885362571
SuperBuddy <36763574+SuperBuddys@users.noreply.github.com>
2018-09-30 18:27:57 +0530

    fibonacciSeries

---
+++ tsIntro/fibonacciSeries.ts
@@ -0,0 +1,17 @@
+var num1 = 0;
+var num2 = 1;
+var num3;
+
+var fibonacciLimit = 10;
+
+console.log(num1);
+console.log(num2);
+
+for( var i = 1; i <= fibonacciLimit; i++)
+	{
+	   num3 = num1 +num2;
+	   num1 = num2;
+	   num2 = num3;
+
+       	  console.log(num3);
+	}
