abbfbef6431dd8b00b65a74517fb7a05bb131ede
RahulMohite <melayer.rahul27@gmail.com>
2019-02-04 12:18:27 +0530

    css box,mock

--- multifile/multiFile/src/app/multifile/multifile.component.ts
+++ multifile/multiFile/src/app/multifile/multifile.component.ts
@@ -1,4 +1,4 @@
-import { HttpEventType } from '@angular/common/http';
+import { HttpEventType, HttpErrorResponse, HttpClient } from '@angular/common/http';
 import { MultifileService } from './multifile.service';
 import { Component, OnInit, Renderer } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormArray, FormControl, NgForm } from '@angular/forms';
@@ -9,159 +9,48 @@ import { FormBuilder, FormGroup, Validators, FormArray, FormControl, NgForm } fr
   styleUrls: ['./multifile.component.css']
 })
 export class MultifileComponent implements OnInit {
-  public documentGrp: FormGroup
+  //public documentGrp: FormGroup
   
-  selectedFile: File=null;
-  // public totalfiles: Array<File> =[];
-  // public totalFileName = [];
-  // public lengthCheckToaddMore =0;
+  myFiles:string [] = [];
+  sMsg:string = '';
+
+ 
   
   constructor(
     private renderer: Renderer,
     private formBuilder: FormBuilder,
-    private multifilesService:MultifileService
+    private multifilesService:MultifileService,
+    private httpClient:HttpClient
   ) { }
 
   ngOnInit() {
-    // this.documentGrp = this.formBuilder.group({
-    //   doc_name: '',
-    //   doc_description: '',
-    //   documentFile:new FormControl(File),
-    //   items: this.formBuilder.array([this.createUploadDocuments()])
-    // });
-
-  // }
-  // createUploadDocuments(): FormGroup {
-  //   return this.formBuilder.group({
-  //     doc_name: '',
-  //     doc_description: '',
-  //     documentFile : File
-  //   });
-  // }
-
-  // get items(): FormArray {
-  //   return this.documentGrp.get('items') as FormArray;
-  // };
-
-  // addItem(): void {
-
-
-//console.log("length is ",this.totalfiles.length);
-//console.log("lengthCheckToaddMore ", this.lengthCheckToaddMore);
-
-// if(this.totalfiles.length!=0)
-// if( this.items.value[0].doc_name != "" && this.items.value[0].doc_description != "" && ((this.lengthCheckToaddMore) === (this.totalfiles.length)) )
-// {
-
-//     this.items.insert(0, this.createUploadDocuments())
-//      this.lengthCheckToaddMore=this.lengthCheckToaddMore+1;
-// }
-//   }
-
-//   removeItem(index: number) {
-
-//    this.totalfiles.splice(index);
-//    this.totalFileName.splice(index);
-//     this.items.removeAt(index);
-//     this.lengthCheckToaddMore=this.lengthCheckToaddMore-1;
-//    // console.log("name are ",this.totalFileName);
-
-//   }
-
-//   public fileSelectionEvent(fileInput: any,oldIndex) {
-
-//     //console.log("oldIndex is ", oldIndex);
-
-//     if (fileInput.target.files && fileInput.target.files[0]) {
-//       var reader = new FileReader();
-//       reader.onload = (event: any) => {
-//       }
-//       if(oldIndex==0)
-//     {
-//       this.totalfiles.unshift((fileInput.target.files[0]))
-//       this.totalFileName.unshift(fileInput.target.files[0].name)
-//     }
-//     else
-//     {
-//       this.totalfiles[oldIndex]=(fileInput.target.files[0]);
-//       this.totalFileName[oldIndex]=fileInput.target.files[0].name
-//     }
-
-//       reader.readAsDataURL(fileInput.target.files[0]);
-//     }
-
-//     if(this.totalfiles.length == 1)
-//     {
-//       this.lengthCheckToaddMore=1;
-//     }
-
-//   }
-
-
-
-
-//   public OnSubmit(formValue: any) {
-
-
-//     let main_form: FormData = new FormData();
-
-//     for(let j=0;j<this.totalfiles.length; j++)
-//     {
-//       console.log("the values is ",<File>this.totalfiles[j]);
-//       console.log("the name is ",this.totalFileName[j]);
-
-//       main_form.append(this.totalFileName[j],<File>this.totalfiles[j])
-//     }
-//     console.log(formValue.items)
-
-
-
-//     //reverseFileNames=this.totalFileName.reverse();
-
-//     let AllFilesObj= []
-
-//     formValue.items.forEach((element, index) => { 
-
-//       console.log("index is ",index);
-//       console.log("element is ", element);
-
-//       let eachObj=
-//       {
-//         'doc_name' : element.doc_name,
-//         'doc_description' : element.doc_description,
-//         'file_name' : this.totalFileName[index]
-//       }
-//       AllFilesObj.push(eachObj); 
-//     });
-
-//     //console.log("the Array data is ",AllFilesObj);
-//     main_form.append("fileInfo",JSON.stringify(AllFilesObj))
-
-//     this.multifilesService.saveFiles(main_form).subscribe(data => {
-//       console.log("result is ", data)
-//     })
-
   }
-  onfileSelected(event){
- console.log("event ", event);
- this.selectedFile = <File> event.target.files[0];
+
+getFileDetails (e) {
+ console.log("e ", e);
+  console.log (e.target.files);
+  for (let i = 0; i < e.target.files.length; i++) { 
+    this.myFiles.push(e.target.files[i]);
   }
-  onUpload(){
-    const fd =new FormData();
-    fd.append('image',this.selectedFile,this.selectedFile.name)
-    this.multifilesService.upload(fd).subscribe(event=>{
- 
- if(event.type === HttpEventType.UploadProgress){
+}
 
- console.log("UploadProgress: "+ Math.round(event.loaded / event.total*100));
-
- }
- else if (event.type === HttpEventType.Response){
-  console.log("event ", event);
- }
- 
-
-    });
+uploadFiles () {
+  const frmData = new FormData();
   
+  for (let i = 0; i < this.myFiles.length; i++) { 
+    frmData.append("file", this.myFiles[i]);
   }
+  
+  this.httpClient.post('http://192.168.15.161:8000/uploadFile', frmData).subscribe(
+    data => {
+      // SHOW A MESSAGE RECEIVED FROM THE WEB API.
+      this.sMsg = data as string;
+      console.log (this.sMsg);
+    },
+    (err: HttpErrorResponse) => {
+      console.log (err.message);    // SHOW ERRORS IF ANY.
+    }
+    //mial varcha code ghyacha ahe tushar ne send kelela
+  );
+}
 }
